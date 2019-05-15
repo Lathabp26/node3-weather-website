abd63e687a892e81a86a6e4c2427d374a69d3bd4
LATHA-PC\latha <latha.bp@diona.com>
2019-05-15 19:37:46 +0530

    some mor new features

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,16 +4,16 @@ const forecast=(latitude,longitude,callback)=>{
 
     const url='https://api.darksky.net/forecast/4e29e3223c3d8d29990a0b698e00581a/'+latitude+','+latitude;
 
-    request({url:url,json:true},(error,response) => {
+    request({url:url,json:true},(error, { body }) => {
         
     if(error){
     callback('Unable to connect',undefined)
-    }else if(response.body.error){
+    }else if(body.error){
       callback('Unable to find the location',undefined)
     }else{
-        callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
-
-    
+        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        
+  
     }})
 
 }
